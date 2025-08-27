24d48d7f2969053db3644af901e1ae95298fb066
Leonardo Lima <leonardo.lima@bitka.com.br>
2025-08-27 17:25:55 -0300

    Definição de pipeline_registry.py

--- iris-kedro-project/src/iris_kedro_project/pipeline_registry.py
+++ iris-kedro-project/src/iris_kedro_project/pipeline_registry.py
@@ -1,16 +1,18 @@
 """Project pipelines."""
-from __future__ import annotations
-
-from kedro.framework.project import find_pipelines
+from __future__ import annotations  # <--- Boa prática, mantida do código original
+from typing import Dict
 from kedro.pipeline import Pipeline
 
+# Importação explícita do nosso pipeline (método do tutorial)
+from iris_kedro_project.pipelines import data_science
 
-def register_pipelines() -> dict[str, Pipeline]:
-    """Register the project's pipelines.
-
-    Returns:
-        A mapping from pipeline names to ``Pipeline`` objects.
-    """
-    pipelines = find_pipelines()
-    pipelines["__default__"] = sum(pipelines.values())
-    return pipelines
+def register_pipelines() -> dict[str, Pipeline]: # <- a anotação de tipo aqui beneficia do __future__
+    """Register the project's pipelines."""
+    
+    # Criação e registo manual (método do tutorial)
+    data_science_pipeline = data_science.create_pipeline()
+    
+    return {
+        "__default__": data_science_pipeline,
+        "ds": data_science_pipeline,
+    }
