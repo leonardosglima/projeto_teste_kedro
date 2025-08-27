816ad833ea3dff25e43277a8666189fb85d94acb
Leonardo Lima <leonardo.lima@bitka.com.br>
2025-08-27 15:58:03 -0300

    Correção de digitação em pipeline.py

--- iris-kedro-project/src/iris_kedro_project/pipelines/data_science/pipeline.py
+++ iris-kedro-project/src/iris_kedro_project/pipelines/data_science/pipeline.py
@@ -3,12 +3,12 @@
 from kedro.pipeline import Pipeline, node, pipeline
 from .nodes import split_data, train_model, predict, report_accuracy
 
-def create_pipeline(**kwarg) -> Pipeline:
+def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
             node(
                 func=split_data,
-                inputs=['iris_raw', 'params:test_size', 'params:random_state', 'params:features', 'params:target_colum'],
+                inputs=['iris_raw', 'params:test_size', 'params:random_state', 'params:features', 'params:target_column'],
                 outputs=['X_train', 'X_test', 'y_train', 'y_test'],
                 name='split_data_node',
             ),
@@ -27,8 +27,9 @@ def create_pipeline(**kwarg) -> Pipeline:
             node(
                 func=report_accuracy,
                 inputs=['predictions', 'y_test'],
-                outputs=None # Este nó não gera saída para o catálogo
+                outputs=None, # Este nó não gera saída para o catálogo
                 name='report_accuracy_node',
             ),
         ]
     )
+
