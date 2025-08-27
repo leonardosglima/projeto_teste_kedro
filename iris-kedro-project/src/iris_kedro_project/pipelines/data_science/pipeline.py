d87564381e63edd44dfb18e91ea6b8221581e332
Leonardo Lima <leonardo.lima@bitka.com.br>
2025-08-27 15:37:57 -0300

    Ajuste na definição do ambiente virtual

--- iris-kedro-project/src/iris_kedro_project/pipelines/data_science/pipeline.py
+++ iris-kedro-project/src/iris_kedro_project/pipelines/data_science/pipeline.py
@@ -1,7 +1,7 @@
 # src/iris_kedro_project/pipelines/data_science/pipeline.py
 
 from kedro.pipeline import Pipeline, node, pipeline
-from .nodes import spli_data, train_model, predict, report_accuracy
+from .nodes import split_data, train_model, predict, report_accuracy
 
 def create_pipeline(**kwarg) -> Pipeline:
     return pipeline(
@@ -11,24 +11,24 @@ def create_pipeline(**kwarg) -> Pipeline:
                 inputs=['iris_raw', 'params:test_size', 'params:random_state', 'params:features', 'params:target_colum'],
                 outputs=['X_train', 'X_test', 'y_train', 'y_test'],
                 name='split_data_node',
-            )
+            ),
             node(
                 func=train_model,
                 inputs=['X_train', 'y_train'],
                 outputs='model',
                 name='train_model_node',
-            )
+            ),
             node(
                 func=predict,
                 inputs=['model', 'X_test'],
                 outputs='predictions',
                 name='predict_node',
-            )
+            ),
             node(
                 func=report_accuracy,
                 inputs=['predictions', 'y_test'],
                 outputs=None # Este nó não gera saída para o catálogo
                 name='report_accuracy_node',
-            )
+            ),
         ]
     )
